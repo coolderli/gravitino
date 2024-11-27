339cb4a3950b74470839cf0f8ff2125cf3fc0bc8
lipeidian <lipeidian@xiaomi.com>
2024-11-27 20:27:24 +0800

    stash

--- flink-connector/flink/src/main/java/org/apache/gravitino/flink/connector/paimon/PaimonPropertiesConverter.java
+++ flink-connector/flink/src/main/java/org/apache/gravitino/flink/connector/paimon/PaimonPropertiesConverter.java
@@ -18,10 +18,63 @@
  */
 package org.apache.gravitino.flink.connector.paimon;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.table.catalog.CommonCatalogOptions;
 import org.apache.gravitino.flink.connector.PropertiesConverter;
+
+import java.util.Map;
+
+import static org.apache.gravitino.catalog.lakehouse.paimon.PaimonCatalogPropertiesMetadata.*;
 
 public class PaimonPropertiesConverter implements PropertiesConverter {
   private PaimonPropertiesConverter() {}
 
+  public static final Map<String, String> PAIMON_CONFIG_TO_GRAVITINO =
+          ImmutableMap.of(
+                  PAIMON_METASTORE,
+                  GRAVITINO_CATALOG_BACKEND,
+                  WAREHOUSE,
+                  WAREHOUSE,
+                  URI,
+                  URI,
+                  PAIMON_JDBC_USER,
+                  GRAVITINO_JDBC_USER,
+                  PAIMON_JDBC_PASSWORD,
+                  GRAVITINO_JDBC_PASSWORD);
+
   public static final PaimonPropertiesConverter INSTANCE = new PaimonPropertiesConverter();
+
+  @Override
+  public Map<String, String> toFlinkCatalogProperties(Map<String, String> gravitinoProperties) {
+    Map<String, String> flinkCatalogProperties = Maps.newHashMap();
+    flinkCatalogProperties.put(CommonCatalogOptions.CATALOG_TYPE.key(), GravitinoPaimonCatalogFactoryOptions.IDENTIFIER);
+
+    gravitinoProperties.forEach(
+            (key, value) -> {
+              String flinkConfigKey = key;
+              if (key.startsWith(PropertiesConverter.FLINK_PROPERTY_PREFIX)) {
+                flinkConfigKey = key.substring(PropertiesConverter.FLINK_PROPERTY_PREFIX.length());
+              }
+              flinkCatalogProperties.put(GRAVITINO_CONFIG_TO_PAIMON.getOrDefault(flinkConfigKey, flinkConfigKey), value);
+            });
+    return flinkCatalogProperties;
+  }
+
+  @Override
+  public Map<String, String> toGravitinoCatalogProperties(Configuration flinkConf) {
+    Map<String, String> gravitinoCatalogProperties = Maps.newHashMap();
+
+    flinkConf.toMap().forEach(
+            (key, value) -> {
+              boolean isGravitinoConfig = PAIMON_CONFIG_TO_GRAVITINO.containsKey(key);
+              if (isGravitinoConfig) {
+                gravitinoCatalogProperties.put(PAIMON_CONFIG_TO_GRAVITINO.get(key), value);
+              } else {
+                gravitinoCatalogProperties.put(FLINK_PROPERTY_PREFIX + key, value);
+              }
+            });
+    return gravitinoCatalogProperties;
+  }
 }
--- flink-connector/flink/src/test/java/org/apache/gravitino/flink/connector/integration/test/paimon/FlinkPaimonCatalogIT.java
+++ flink-connector/flink/src/test/java/org/apache/gravitino/flink/connector/integration/test/paimon/FlinkPaimonCatalogIT.java
@@ -21,17 +21,25 @@ package org.apache.gravitino.flink.connector.integration.test.paimon;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableMap;
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.table.api.ResultKind;
+import org.apache.flink.table.api.TableResult;
 import org.apache.flink.table.catalog.CatalogDescriptor;
 import org.apache.flink.table.catalog.CommonCatalogOptions;
 import org.apache.gravitino.Catalog;
 import org.apache.gravitino.catalog.lakehouse.paimon.PaimonCatalogPropertiesMetadata;
+import org.apache.gravitino.exceptions.NoSuchCatalogException;
 import org.apache.gravitino.flink.connector.integration.test.FlinkCommonIT;
+import org.apache.gravitino.flink.connector.integration.test.utils.TestUtils;
+import org.apache.gravitino.flink.connector.paimon.GravitinoPaimonCatalog;
 import org.apache.gravitino.flink.connector.paimon.GravitinoPaimonCatalogFactoryOptions;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Optional;
 
 @Tag("gravitino-docker-test")
 public class FlinkPaimonCatalogIT extends FlinkCommonIT {
@@ -90,5 +98,71 @@ public class FlinkPaimonCatalogIT extends FlinkCommonIT {
     CatalogDescriptor catalogDescriptor = CatalogDescriptor.of(catalogName, configuration);
     tableEnv.createCatalog(catalogName, catalogDescriptor);
     Assertions.assertTrue(metalake.catalogExists(catalogName));
+
+    Catalog catalog = metalake.loadCatalog(catalogName);
+    Assertions.assertEquals("lakehouse-paimon", catalog.provider());
+    Assertions.assertEquals("gravitino_paimon", catalog.name());
+    Assertions.assertEquals("hive", catalog.properties().get("catalog-backend"));
+    Assertions.assertEquals(hiveMetastoreUri, catalog.properties().get("uri"));
+    Assertions.assertEquals(warehouse + "/gravitino_paimon", catalog.properties().get("warehouse"));
+
+    // test list catalogs
+    String[] catalogs = metalake.listCatalogs();
+    Assertions.assertTrue(Arrays.asList(catalogs).contains(catalogName));
+
+    // change catalog
+    tableEnv.useCatalog(catalogName);
+    Assertions.assertEquals(catalogName, tableEnv.getCurrentCatalog());
+
+    // test drop catalog
+    tableEnv.useCatalog(DEFAULT_CATALOG);
+    tableEnv.executeSql("drop catalog " + catalogName);
+    Assertions.assertFalse(metalake.catalogExists(catalogName));
+    Assertions.assertThrows(
+        NoSuchCatalogException.class, () -> metalake.loadCatalog(catalogName));
+  }
+
+  @Test
+  public void testCreatePaimonCatalogUsingSql() throws Exception {
+    tableEnv.useCatalog(DEFAULT_CATALOG);
+
+    String catalogName = "test_paimon_catalog_using_sql";
+    Assertions.assertFalse(metalake.catalogExists(catalogName));
+    String sql = String.format("create catalog %s "
+            + " with ("
+            + "'type'='gravitino-paimon',"
+            + "'catalog-backend'='hive',"
+            + "'uri'='%s',"
+            + "'warehouse'='%s'"
+            + ")", catalogName, hiveMetastoreUri, warehouse + "/test_paimon_catalog_using_sql");
+    TableResult result = tableEnv.executeSql(sql);
+    TestUtils.assertTableResult(result, ResultKind.SUCCESS);
+    Assertions.assertTrue(metalake.catalogExists(catalogName));
+  }
+
+  @Test
+  public void testLoadPaimonCatalogFromGravitino() {
+    String catalogName = "test_paimon_catalog_from_gravitino";
+    Catalog catalog = metalake.createCatalog(
+            catalogName,
+            Catalog.Type.RELATIONAL,
+            "lakehouse-paimon",
+            "test comment",
+            ImmutableMap.of(
+                    "uri",
+                    hiveMetastoreUri,
+                    "warehouse",
+                    warehouse + "/test_paimon_catalog_from_gravitino",
+                    "catalog-backend",
+                    "hive")
+    );
+    Assertions.assertNotNull(catalog);
+    Assertions.assertTrue(metalake.catalogExists(catalogName));
+
+    Optional<org.apache.flink.table.catalog.Catalog> flinkCatalog = tableEnv.getCatalog(catalogName);
+    Assertions.assertTrue(flinkCatalog.isPresent());
+    Assertions.assertInstanceOf(GravitinoPaimonCatalog.class, flinkCatalog.get());
+    GravitinoPaimonCatalog gravitinoPaimonCatalog = (GravitinoPaimonCatalog) flinkCatalog.get();
+    Assertions.assertEquals(catalogName, gravitinoPaimonCatalog.getName());
   }
 }
